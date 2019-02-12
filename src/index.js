b67af06d42c3bf1ada14c262a0b0f118e4c16f1c
kumaa15 <Ajay.Kumar@in.pega.com>
2019-02-12 23:46:32 +0530

    initial react setup with babel and webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom"
+import React from "react"
+import Game  from "./js/components/Game.jsx";
+import './css/style.css';
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
